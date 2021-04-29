3057211356ede66ec0e6427912fbb5e1c6193026
Onur Çelik <ourcelik@icloud.com>
2021-04-29 19:39:49 +0300

    LikeKind ,Match Controller added

---
+++ SocialAppWebApi/Controllers/LikeKindsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialAppWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikeKindsController : ControllerBase
+    {
+        ILikeKindService _likeKindService;
+
+        public LikeKindsController(ILikeKindService likeKindService)
+        {
+            _likeKindService = likeKindService;
+        }
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAllKindsAsync()
+        {
+            var data =await _likeKindService.GetAllKindsAsync();
+            return data.Success ? Ok(data) : BadRequest(data);
+        }
+    }
+}
---
+++ SocialAppWebApi/Controllers/MatchesController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialAppWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchesController : ControllerBase
+    {
+        IMatchService _matchService;
+
+        public MatchesController(IMatchService matchService)
+        {
+            _matchService = matchService;
+        }
+
+        [HttpGet("getall")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _matchService.GetAllMatchesAsync();
+            return data.Success ? Ok(data) : BadRequest(data);
+        }
+        [HttpGet("getmatchbyid/{id}")]
+        public async Task<IActionResult> GetMatchByIdAsync(int id)
+        {
+            var data = await _matchService.GetMatchByIdAsync(id);
+            return data.Success ? Ok(data) : BadRequest(data);
+        }
+        [HttpGet("getmatchbylevel/{id}")]
+        public async Task<IActionResult> GetAll(int id)
+        {
+            var data = await _matchService.GetMatchesByLevelIdAsync(id);
+            return data.Success ? Ok(data) : BadRequest(data);
+        }
+
+        [HttpGet("getmatchbyuserid/{id}")]
+        public async Task<IActionResult> GetMatchesByUserIdAsync(int id)
+        {
+            var data = await _matchService.GetMatchesByUserIdAsync(id);
+            return data.Success ? Ok(data) : BadRequest(data);
+        }
+
+    }
+}
