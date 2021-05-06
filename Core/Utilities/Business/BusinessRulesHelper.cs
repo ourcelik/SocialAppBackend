2fbd9c094a397229383915621d042bbda4c3d20c
Onur Çelik <ourcelik@icloud.com>
2021-05-06 13:37:58 +0300

    BusinessRulesHelper Added

---
+++ Core/Utilities/Business/BusinessRulesHelper.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRulesHelper
+    {
+        public static IResult RunLogics(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
+    }
+}
